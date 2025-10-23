86d353fe5669508c194d36f3cc6c6dbfdd9bbc5f
Miles <wangle2071@163.com>
2025-10-23 11:21:13 +0800

    v2025102311121

--- app/home/page.js
+++ app/home/page.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="mt-5 ml-5">This is home page</h1>
     </div>
   )
 }
--- middleware.js
+++ middleware.js
@@ -1,11 +1,7 @@
-// middleware.js
 import { NextResponse } from 'next/server'
-
-export const middleware = (request) => {
-  return NextResponse.redirect(new URL('/home', request.url))
+ 
+export function middleware(request) {
+  if (request.nextUrl.pathname.startsWith('/about')) {
+    return NextResponse.rewrite(new URL('/home', request.url))
+  }
 }
-
-export const config = {
-  matcher: '/about/:path*',
-}
-
